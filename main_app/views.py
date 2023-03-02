bb345b198d68c0be05099363fd1e770d826667cd
Timm Schoenborn <timmschoenborn@gmail.com>
2023-03-02 14:30:28 -0500

    cats index actually using database cats instead of dummy data

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from .models import Cat
 
 # temporary cats for building templates
-cats = [
-    {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-    {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-    {'name': 'Tubs', 'breed': 'ragdoll', 'description': 'chunky lil guy', 'age': 0},
-]
+# cats = [
+#     {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+#     {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+#     {'name': 'Tubs', 'breed': 'ragdoll', 'description': 'chunky lil guy', 'age': 0},
+# ]
 
 # Create your views here.
 # view functions match urls to code (like controllers in Express)
@@ -21,4 +22,6 @@ def about(request):
 def cats_index(request):
     # just like we passed data to our templates in express
     # we pass data to our templates through our view functions
+    # we can gather relations from SQL using our model methods
+    cats = Cat.objects.all()
     return render(request, 'cats/index.html', { 'cats': cats })
