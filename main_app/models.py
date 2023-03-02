a5638a4433f8cbb89ba5f97d1b05a4ab3c25332c
Timm Schoenborn <timmschoenborn@gmail.com>
2023-03-02 14:01:59 -0500

    adds cat model and dunder str method for cat

--- main_app/models.py
+++ main_app/models.py
@@ -6,3 +6,7 @@ class Cat(models.Model):
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+
+    # dunder str method return cat name
+    def __str__(self):
+        return self.name
