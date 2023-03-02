23449a4ebfa70da6079ef36f6b4a8facfc92db51
Timm Schoenborn <timmschoenborn@gmail.com>
2023-03-02 11:50:02 -0500

    adds base html and styling

---
+++ main_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # using an empty string here makes this our root route
+    # views.home refers to a view that renders a file
+    # the name='home' kwarg gives the route a name
+    # naming routes is optional, but best practices
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about')
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+# view functions match urls to code (like controllers in Express)
+# define our home view function
+def home(request):
+    return render(request, 'home.html')
+
+# about route
+def about(request):
+    return render(request, 'about.html')
