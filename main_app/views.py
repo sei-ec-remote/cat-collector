194ec3cfefad53f76184b7971b12fa944140e0ea
Timm Schoenborn <timmschoenborn@gmail.com>
2023-03-06 15:09:00 -0500

    adds auth and protection to resources

--- main_app/views.py
+++ main_app/views.py
@@ -7,6 +7,15 @@ from .forms import FeedingForm
 import uuid #python package for creating unique identifiers
 import boto3 #what we'll use to connect to s3
 from django.conf import settings
+# imports for signing up
+# we want to automatically log in signed up users
+from django.contrib.auth import login
+# we want to use the builtin form for our custom view for sign up
+from django.contrib.auth.forms import UserCreationForm
+# Import the login_required decorator
+from django.contrib.auth.decorators import login_required
+# Import the mixin for class-based views
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 AWS_ACCESS_KEY = settings.AWS_ACCESS_KEY
 AWS_SECRET_ACCESS_KEY = settings.AWS_SECRET_ACCESS_KEY
@@ -24,15 +33,19 @@ def about(request):
     return render(request, 'about.html')
 
 # index route for cats
+@login_required
 def cats_index(request):
     # just like we passed data to our templates in express
     # we pass data to our templates through our view functions
     # we can gather relations from SQL using our model methods
-    cats = Cat.objects.all()
+    # cats = Cat.objects.all()
+    cats = Cat.objects.filter(user=request.user)
+
     return render(request, 'cats/index.html', { 'cats': cats })
 
 # detail route for cats
 # cat_id is defined, expecting an integer, in our url
+@login_required
 def cats_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
 
@@ -44,27 +57,33 @@ def cats_detail(request, cat_id):
     feeding_form = FeedingForm()
     return render(request, 'cats/detail.html', { 'cat': cat, 'feeding_form': feeding_form, 'toys': toys_cat_doesnt_have })
 
-class CatCreate(CreateView):
+class CatCreate(LoginRequiredMixin, CreateView):
     model = Cat
     # the fields attribute is required for a createview. These inform the form
-    fields = '__all__'
+    # fields = '__all__'
     # we could also have written our fields like this:
-    # fields = ['name', 'breed', 'description', 'age']
+    fields = ['name', 'breed', 'description', 'age']
     # we need to add redirects when we make a success
     # success_url = '/cats/{cat_id}'
     # or, we could redirect to the index page if we want
     # success_url = '/cats'
     # what django recommends, is adding a get_absolute_url method to the model
+    def form_valid(self, form):
+        # we can assign the logged in user's data(id) to the cat's create form
+        form.instance.user = self.request.user
 
-class CatUpdate(UpdateView):
+        return super().form_valid(form)
+
+class CatUpdate(LoginRequiredMixin, UpdateView):
     model = Cat
     # let's use custom fields to disallow renaming a cat
     fields = ['breed', 'description', 'age']
 
-class CatDelete(DeleteView):
+class CatDelete(LoginRequiredMixin, DeleteView):
     model = Cat
     success_url = '/cats/'
 
+@login_required
 def add_feeding(request, cat_id):
     # create a ModelForm instance from the data in request.POST
     form = FeedingForm(request.POST)
@@ -77,26 +96,28 @@ def add_feeding(request, cat_id):
         new_feeding.save()
     return redirect('detail', cat_id=cat_id)
 
+@login_required
 def assoc_toy(request, cat_id, toy_id):
     Cat.objects.get(id=cat_id).toys.add(toy_id)
     return redirect('detail', cat_id=cat_id)
 
+@login_required
 def unassoc_toy(request, cat_id, toy_id):
     Cat.objects.get(id=cat_id).toys.remove(toy_id)
     return redirect('detail', cat_id=cat_id)
 
 # ToyList
-class ToyList(ListView):
+class ToyList(LoginRequiredMixin, ListView):
     model = Toy
     template_name = 'toys/index.html'
 
 # ToyDetail
-class ToyDetail(DetailView):
+class ToyDetail(LoginRequiredMixin, DetailView):
     model = Toy
     template_name = 'toys/detail.html'
 
 # ToyCreate
-class ToyCreate(CreateView):
+class ToyCreate(LoginRequiredMixin, CreateView):
     model = Toy
     fields = ['name', 'color']
 
@@ -108,16 +129,17 @@ class ToyCreate(CreateView):
         return super().form_valid(form)
 
 # ToyUpdate
-class ToyUpdate(UpdateView):
+class ToyUpdate(LoginRequiredMixin, UpdateView):
     model = Toy
     fields = ['name', 'color']
 
 # ToyDelete
-class ToyDelete(DeleteView):
+class ToyDelete(LoginRequiredMixin, DeleteView):
     model = Toy
     success_url = '/toys/'
 
 # view for adding photos
+@login_required
 def add_photo(request, cat_id):
     # photo-file will be the name attribute of our form input
     # input type will be file
@@ -147,3 +169,26 @@ def add_photo(request, cat_id):
             return redirect('detail', cat_id=cat_id)
     # upon success redirect to detail page 
     return redirect('detail', cat_id=cat_id)
+
+# view for signup
+def signup(request):
+    # this view is going to be like our class based views
+    # because this is going to be able to handle a GET and a POST request
+    error_message = ''
+    if request.method == 'POST':
+        # this is how to create a user form object that includes data from the browser
+        form = UserCreationForm(request.POST)
+        # now we check validity of the form, and handle our success and error situations
+        if form.is_valid():
+            # we'll add the user to the database
+            user = form.save()
+            # then we'll log the user in
+            login(request, user)
+            # redirect to our index page
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    # a bad POST or GET request will render signup.html with an empty form
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
